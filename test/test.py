1fdeb842392c28dc0abd97a9148ebbecce508ec9
lhf2018 <1146953160@qq.com>
2020-04-30 10:51:18 +0800

    特征值数据集

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
-file1=open("F:\\pycharmproject\\GraduationProject\\data\\feature_data - 副本.txt",mode='r')
-file2=open("F:\\pycharmproject\\GraduationProject\\data\\feature_data - 副本1.txt",mode='w')
+file1=open("F:\\pycharmproject\\GraduationProject\\data\\feature_data_statistic.txt",mode='r')
+file2=open("F:\\pycharmproject\\GraduationProject\\data\\feature_data_statistic_csv.csv",mode='w')
 for f in file1.readlines():
     f=f.replace(" ",',')
     file2.write(f)
